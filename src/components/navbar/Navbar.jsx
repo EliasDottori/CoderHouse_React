86437f9e21c44c357af88f86904e2c6d3b21dfb4
Elias Dottori <elias.dottori@hotmail.com>
2023-04-29 12:15:21 -0300

    v1.1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 
 const navbar = () => {
   return (
     <div>
-        <nav>
-            <a href="../index.html" className="logo"></a>
-            <div className="containerBtnNav">
-                <button id="btnOfertaHTML" className="btnNone btnNav">
-                Vinos Seleccionados
-                </button>
-                <button id="btnCatalogoHTML" className="btnNone btnNav">
-                Catalogo completo
-                </button>
-            </div>
-            <div className="containerOpcion">
-                <i className="fa-solid fa-user"></i>
+      <nav>
+        <a title="inicio" href="../index.html" className="logo"></a>
+        <div className="containerBtnNav">
+          <button id="btnOfertaHTML" className="btnNone btnNav">
+            Vinos Seleccionados
+          </button>
+          <button id="btnCatalogoHTML" className="btnNone btnNav">
+            Catalogo completo
+          </button>
+        </div>
+        <div className="containerOpcion">
+          <i className="fa-solid fa-user"></i>
 
-                <button
-                id="btnCarritoHTML"
-                className="btnNone"
-                href=""
-                >
-                    <i className="fa-solid fa-cart-shopping"></i>
-                </button>
-            </div>
-        </nav>
-        <div className="expansibleNav">
+          <button id="btnCarritoHTML" className="btnNone" href="">
+            <i className="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+      </nav>
+      <div className="expansibleNav">
         <div className="selectNameContainer">
           <h4>Ordenar por Precio</h4>
-          <select name="select" defaultValue="0" id="selectOrden" className="expansibleOption">
-            <option value="0" >
-              Seleccione
-            </option>
+          <select
+            name="select"
+            defaultValue="0"
+            id="selectOrden"
+            className="expansibleOption"
+          >
+            <option value="0">Seleccione</option>
             <option value="1">Menor a Mayor</option>
             <option value="2">Mayor a Menor</option>
           </select>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
